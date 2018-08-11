a587ef1de407783bcf87aa301b66af0850b31fa7
ivan9105 <ivan9105@bk.ru>
2018-08-11 18:33:37 +0400

    xls part tests #4

--- src/test/java/com/simple_bot/AbstractXPathTest.java
+++ src/test/java/com/simple_bot/AbstractXPathTest.java
@@ -1,4 +1,42 @@
 package com.simple_bot;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
+
+import static javax.xml.parsers.DocumentBuilderFactory.newInstance;
+import static javax.xml.xpath.XPathConstants.NODESET;
+import static org.junit.Assert.assertEquals;
+
 public interface AbstractXPathTest {
+    default Document getDocument(String path) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory dbf = newInstance();
+        dbf.setValidating(false);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        return db.parse(this.getClass().getResourceAsStream(path));
+    }
+
+    default XPath getXPath() {
+        return XPathFactory.newInstance().newXPath();
+    }
+
+    default NodeList evaluateNodeList(Document doc, XPath xpath, String expression) throws XPathExpressionException {
+        return (NodeList) xpath.evaluate(expression, doc, NODESET);
+    }
+
+    default void checkNodeListAttributes(NodeList nodeList, String attributeName, String... expectedValues) {
+        int currentIndex = 0;
+        for (String expectedValue : expectedValues) {
+            assertEquals(nodeList.item(currentIndex).getAttributes().getNamedItem(attributeName).getNodeValue(), expectedValue);
+            currentIndex++;
+        }
+    }
 }
--- src/test/java/com/simple_bot/xpath/XPathTest.java
+++ src/test/java/com/simple_bot/xpath/XPathTest.java
@@ -2,24 +2,16 @@ package com.simple_bot.xpath;
 
 import com.simple_bot.AbstractXPathTest;
 import com.simple_bot.AbstractXSLTTest;
-import org.apache.commons.io.IOUtils;
-import org.junit.Assert;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import java.io.FileInputStream;
 import java.io.IOException;
 
-import static javax.xml.parsers.DocumentBuilderFactory.newInstance;
-import static javax.xml.xpath.XPathConstants.NODESET;
 import static org.junit.Assert.assertEquals;
 
 public class XPathTest implements AbstractXSLTTest, AbstractXPathTest {
@@ -30,26 +22,36 @@ public class XPathTest implements AbstractXSLTTest, AbstractXPathTest {
 
     @Test
     public void test_SelectingNodes() throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
-        //TOdo common class
-        DocumentBuilderFactory dbf = newInstance();
-        dbf.setValidating(false);
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        Document doc = db.parse(this.getClass().getResourceAsStream("/xsl/xpath/select_nodes/select_nodes.xml"));
-        XPathFactory factory = XPathFactory.newInstance();
-        XPath xpath = factory.newXPath();
+        Document doc = getDocument("/xsl/xpath/select_nodes/select_nodes.xml");
+        XPath xpath = getXPath();
 
-        NodeList nodeList = (NodeList) xpath.evaluate("//*[@name='Ivan']", doc, NODESET);
-        assertEquals(2, nodeList.getLength());
-        assertEquals(nodeList.item(0).getAttributes().getNamedItem("name").getNodeValue(), "Ivan");
-        assertEquals(nodeList.item(1).getAttributes().getNamedItem("name").getNodeValue(), "Ivan");
-        assertEquals(nodeList.item(0).getAttributes().getNamedItem("surname").getNodeValue(), "Fedorov");
-        assertEquals(nodeList.item(1).getAttributes().getNamedItem("surname").getNodeValue(), "Zinin");
-        //Todo
+        NodeList nodeList = evaluateNodeList(doc, xpath, "//*[@name='Ivan']");
+        checkNodeListAttributes(nodeList, "name", "Ivan", "Ivan");
+        checkNodeListAttributes(nodeList, "surname", "Fedorov", "Zinin");
+
+        nodeList = evaluateNodeList(doc, xpath, "//*[contains(@name,'a')]");
+        checkNodeListAttributes(nodeList, "name", "Ivan", "Ivan", "Oksana", "Maria");
+
+        nodeList = evaluateNodeList(doc, xpath, "//*[@surname]");
+        checkNodeListAttributes(nodeList, "surname", "Fedorov", "Zinin", "Chernov", "Ivanova", "Testova");
+
+        nodeList = evaluateNodeList(doc, xpath, "//*[@name and @surname]");
+        checkNodeListAttributes(nodeList, "surname", "Fedorov", "Zinin", "Chernov", "Ivanova");
     }
 
     @Test
-    public void test_SelectingUnknownNodes() {
-        //Todo
+    public void test_SelectingUnknownNodes() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException {
+        Document doc = getDocument("/xsl/xpath/select_nodes/select_nodes.xml");
+        XPath xpath = getXPath();
+
+        NodeList nodeList = evaluateNodeList(doc, xpath, "//woman");
+        assertEquals(3, nodeList.getLength());
+
+        nodeList = evaluateNodeList(doc, xpath, "/entries/woman");
+        assertEquals(3, nodeList.getLength());
+
+        nodeList = evaluateNodeList(doc, xpath, "/entries/*");
+        assertEquals(7, nodeList.getLength());
     }
 
     @Test
